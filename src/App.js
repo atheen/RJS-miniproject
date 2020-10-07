4d5776b953f640c0509fb19be89b42960397f688
Atheen Aleesa <atheenaleesa@Atheens-MacBook-Pro.local>
2020-10-07 11:33:08 +0300

    completed the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+//components
+import MoviesList from './List.js'
+import Input from './Input.js'
 
 function App() {
+  const [watchList,setWatchList] = useState([])
+  const [watched,setWatched] = useState([])
+
+  const moveToWatched = m => {
+    let newWatchList = watchList.filter(movie => movie !== m);
+    setWatchList(newWatchList);
+    setWatched(watched.concat([m]));
+  };
+
+  const moveToWatchList = m => {
+    let newWatched = watched.filter(movie => movie !== m);
+    setWatched(newWatched);
+    setWatchList(watchList.concat([m]));
+  };
+
+  const deleteMovie = m => {
+    let newWatched = watched.filter(movie => movie !== m);
+    setWatched(newWatched);
+    let newWatchList = watchList.filter(movie => movie !== m);
+    setWatchList(newWatchList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input watchList={watchList} setWatchList={setWatchList} />
+      <MoviesList list={watchList} move={moveToWatched} moveTo="Watched" deleteMovie={deleteMovie} listType="WatchList" />
+      <h1>------------------------------------</h1>
+      <MoviesList list={watched} move={moveToWatchList} moveTo="Unwatch" deleteMovie={deleteMovie} listType="Watched" />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,13 @@
+const movies = [
+  {
+    name: "the joker",
+    watched: true
+  },
+  {
+    name: "taken",
+    watched: false
+  },
+
+];
+
+export default movies;
